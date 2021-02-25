883ce0fc6174e06adebce1072e4cc7164e455d8c
Zey413 <78432083+Zey413@users.noreply.github.com>
2021-02-25 11:19:20 +0800

    Update loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -13,7 +13,7 @@ class Tacotron2Loss(nn.Module):
 
         mel_out, mel_out_postnet, gate_out, _ = model_output
         gate_out = gate_out.view(-1, 1)
-        mel_loss = nn.MSELoss()(mel_out, mel_target) + \
-            nn.MSELoss()(mel_out_postnet, mel_target)
-        gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
+        mel_loss = nn.MSELoss()(mel_out, mel_target) + \                #mel谱loss由两个构成，第一个是Decoder得到的，第二个是再经过Postnet（一些CNN）后得到的更精细的mel谱
+            nn.MSELoss()(mel_out_postnet, mel_target)                   #两个mel谱都计算损失后加和
+        gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)       #Stop Token 是否应该停下来 二分类
         return mel_loss + gate_loss
