bd6822877eb9aad7e7e06ebf21c78848d21cc401
Zey413 <78432083+Zey413@users.noreply.github.com>
2021-02-27 03:58:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,10 @@ from torch import nn
 from torch.nn import functional as F
 from layers import ConvNorm, LinearNorm
 from utils import to_gpu, get_mask_from_lengths
+import numpy as np
+from transformers import *
+import logging
+
 
 
 class LocationLayer(nn.Module):
@@ -147,58 +151,66 @@ class Postnet(nn.Module):
 
 
 class Encoder(nn.Module):
-    """Encoder module:
-        - Three 1-d convolution banks
-        - Bidirectional LSTM
-    """
     def __init__(self, hparams):
         super(Encoder, self).__init__()
+        self.fc = nn.Linear(1024, 768, bias = False)
+        
+    def forward(self, text_inputs, attention_mask):
+        with torch.no_grad():                      #将不会track 梯度
+            
+# class Encoder(nn.Module):
+#     """Encoder module:
+#         - Three 1-d convolution banks
+#         - Bidirectional LSTM
+#     """
+#     def __init__(self, hparams):
+#         super(Encoder, self).__init__()
 
-        convolutions = []
-        for _ in range(hparams.encoder_n_convolutions):
-            conv_layer = nn.Sequential(
-                ConvNorm(hparams.encoder_embedding_dim,
-                         hparams.encoder_embedding_dim,
-                         kernel_size=hparams.encoder_kernel_size, stride=1,
-                         padding=int((hparams.encoder_kernel_size - 1) / 2),
-                         dilation=1, w_init_gain='relu'),
-                nn.BatchNorm1d(hparams.encoder_embedding_dim))
-            convolutions.append(conv_layer)
-        self.convolutions = nn.ModuleList(convolutions)
+#         convolutions = []
+#         for _ in range(hparams.encoder_n_convolutions):
+#             conv_layer = nn.Sequential(
+#                 ConvNorm(hparams.encoder_embedding_dim,
+#                          hparams.encoder_embedding_dim,
+#                          kernel_size=hparams.encoder_kernel_size, stride=1,
+#                          padding=int((hparams.encoder_kernel_size - 1) / 2),
+#                          dilation=1, w_init_gain='relu'),
+#                 nn.BatchNorm1d(hparams.encoder_embedding_dim))
+#             convolutions.append(conv_layer)
+#         self.convolutions = nn.ModuleList(convolutions)
 
-        self.lstm = nn.LSTM(hparams.encoder_embedding_dim,
-                            int(hparams.encoder_embedding_dim / 2), 1,
-                            batch_first=True, bidirectional=True)
+#         self.lstm = nn.LSTM(hparams.encoder_embedding_dim,
+#                             int(hparams.encoder_embedding_dim / 2), 1,
+#                             batch_first=True, bidirectional=True)
 
-    def forward(self, x, input_lengths):
-        for conv in self.convolutions:
-            x = F.dropout(F.relu(conv(x)), 0.5, self.training)
+#     def forward(self, x, input_lengths):
+#         for conv in self.convolutions:
+#             x = F.dropout(F.relu(conv(x)), 0.5, self.training)
 
-        x = x.transpose(1, 2)
+#         x = x.transpose(1, 2)
 
-        # pytorch tensor are not reversible, hence the conversion
-        input_lengths = input_lengths.cpu().numpy()
-        x = nn.utils.rnn.pack_padded_sequence(
-            x, input_lengths, batch_first=True)
+#         # pytorch tensor are not reversible, hence the conversion
+#         input_lengths = input_lengths.cpu().numpy()
+#         x = nn.utils.rnn.pack_padded_sequence(
+#             x, input_lengths, batch_first=True)
 
-        self.lstm.flatten_parameters()
-        outputs, _ = self.lstm(x)
+#         self.lstm.flatten_parameters()
+#         outputs, _ = self.lstm(x)
 
-        outputs, _ = nn.utils.rnn.pad_packed_sequence(
-            outputs, batch_first=True)
+#         outputs, _ = nn.utils.rnn.pad_packed_sequence(
+#             outputs, batch_first=True)
 
-        return outputs
+#         return outputs
 
-    def inference(self, x):
-        for conv in self.convolutions:
-            x = F.dropout(F.relu(conv(x)), 0.5, self.training)
+#     def inference(self, x):
+#         for conv in self.convolutions:
+#             x = F.dropout(F.relu(conv(x)), 0.5, self.training)
 
-        x = x.transpose(1, 2)
+#         x = x.transpose(1, 2)
 
-        self.lstm.flatten_parameters()
-        outputs, _ = self.lstm(x)
+#         self.lstm.flatten_parameters()
+#         outputs, _ = self.lstm(x)
 
-        return outputs
+#         return outputs
 
 
 class Decoder(nn.Module):
@@ -497,12 +509,13 @@ class Tacotron2(nn.Module):
         return outputs
 
     def forward(self, inputs):
-        text_inputs, text_lengths, mels, max_len, output_lengths = inputs
+        text_inputs, attention_mask, text_lengths, mels, max_len, output_lengths = inputs     #有两个是ground_truth
         text_lengths, output_lengths = text_lengths.data, output_lengths.data
 
-        embedded_inputs = self.embedding(text_inputs).transpose(1, 2)
+#         embedded_inputs = self.embedding(text_inputs).transpose(1, 2)
 
-        encoder_outputs = self.encoder(embedded_inputs, text_lengths)
+#         encoder_outputs = self.encoder(embedded_inputs, text_lengths)
+        encoder_outputs = self.encoder(text_inputs, attention_mask)
 
         mel_outputs, gate_outputs, alignments = self.decoder(
             encoder_outputs, mels, memory_lengths=text_lengths)
